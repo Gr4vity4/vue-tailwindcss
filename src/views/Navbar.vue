2647d75c45f78fba674f2293b46843f3ebe0bfeb
Gr4vity4 <prawared.bw@gmail.com>
2019-07-18 12:44:25 +0700

    Update Navbar Styles.

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -127,6 +127,44 @@
       </div>
     </nav>
 
+    <div class="mt-8 mb-8"></div>
+
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500">
+      <div class="flex items-center flex-shrink-0 text-white mr-6 p-6">
+        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
+          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>
+        </svg>
+        <span class="font-semibold text-xl tracking-tight">Company</span>
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white mr-6" @click="navbarToggle3">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div :class="{
+      'w-full block flex-grow lg:flex lg:items-center lg:w-auto': this.navbarCollapsed3 === false,
+      'w-full block flex-grow lg:flex lg:items-center lg:w-auto hidden': this.navbarCollapsed3 === true
+      }">
+        <div class="text-sm lg:w-full lg:flex lg:justify-start items-center h-20">
+          <a href="#responsive-header" class="bg-indigo-600 text-white h-20 flex items-center px-8">
+            Menu1
+          </a>
+          <a href="#responsive-header" class="text-white h-20 flex items-center px-8 hover:bg-indigo-600 bg-teal-500">
+            Menu2
+          </a>
+          <a href="#responsive-header" class="text-white h-20 flex items-center px-8 hover:bg-indigo-600 bg-teal-500">
+            Menu3
+          </a>
+          <a href="#responsive-header" class="text-white h-20 flex items-center px-8 hover:bg-indigo-600 bg-teal-500">
+            Menu4
+          </a>
+          <input class="mr-4 w-full appearance-none block bg-gray-200 text-gray-700 py-3 px-4 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Search">
+        </div>
+      </div>
+    </nav>
+
   </div>
 </template>
 
